34099de04e01c0e45d812dc0a221d5ade7ccf371
kazuhi-ra <kazuhira4126@gmail.com>
2021-03-27 15:47:31 +0900

    静的ファイルを置いてみる

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
 const HomePage = () => {
-  return <div>Welcome to Next.js! by TS</div>
+  return (
+    <div>
+      <div>Welcome to Next.js! by TS</div>
+      <img src='./images/kazuhira.jpeg' alt='kazuhi-raのアイコン' width="200" />
+    </div>
+  )
 }
 
 export default HomePage
