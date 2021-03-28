7a12f2ac4181d672f0e73099fd7bf7f8b3e34b74
kazuhi-ra <kazuhira4126@gmail.com>
2021-03-28 22:55:53 +0900

    getInitialPropsを使ってみる

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
-const HomePage = () => {
+import { NextPage } from 'next'
+
+type Props = { title: string }
+
+const HomePage: NextPage<Props> = ({ title }) => {
+
   return (
     <div>
       <div>Welcome to Next.js! by TS</div>
-      <img src='./images/kazuhira.jpeg' alt='kazuhi-raのアイコン' width="200" />
+      <img src='./images/kazuhira.jpeg' alt='kazuhi-raのアイコン' width='200' />
     </div>
   )
 }
 
 export default HomePage
+
+HomePage.getInitialProps = async () => {
+  return { title: 'Top' }
+}
